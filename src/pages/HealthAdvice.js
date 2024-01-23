e538570fb97a6be1f5abfb07c5a396086c93a6ee
rijan-1 <rijanrai125@gmail.com>
2024-01-23 14:54:05 +0000

    finished healtha dvice

--- src/pages/HealthAdvice.js
+++ src/pages/HealthAdvice.js
@@ -4,7 +4,7 @@ import {GeoCodingApi, CurrentAirQualityAPI} from '../WeatherService'
 import { GiTumbleweed } from "react-icons/gi";
 import { GiMoldova } from "react-icons/gi";
 import { GiGrass } from "react-icons/gi";
-
+import { useNavigate } from 'react-router-dom';
 import { useContext, useState, useEffect } from 'react';
 import { MyContext } from '../App';
 import {CurrentWeather} from '../WeatherService'
@@ -12,18 +12,24 @@ export const HealthAdvice = () => {
   const [CoordnatesState,setCoordnatesState] = useState({lat: 51.5073219, lon: -0.1276474})
   const [currentAirQualityState, setAirQualityState] = useState({})
 
-  const{ GlobalCityName, setGlobalCityName,units, setUnits, HealthTemp, setHealthTemp,HealthAirQuality, setHealthAirQuality} = useContext(MyContext)
-
+  const{ GlobalCityName, setGlobalCityName,units, isLoggedIn, setIsLoggedIn,HealthAirQuality, setHealthAirQuality} = useContext(MyContext)
+const navigate = useNavigate()
   const [ColdurticariaState, setColdurticariaState] = useState('NOT WORKING')
   const [ColorColdurticariaState, setColorColdurticariaState] = useState('white')
   const [FluState, setFluState] = useState('NOT WORKING')
+  const [ColorFluState, setColorFluState] = useState('NOT WORKING')
   const [HypothermiaState, setHyprothermia] =useState('NOT WORKING')
+  const [ColorHypothermiaState, setColorHyprothermia] =useState('white')
   const [heatcramps, setheatcramps]= useState('NOT WORKING')
+  const [Colorheatcramps, setColorheatcramps]= useState('white')
   const [asthma, setAsthma] = useState('NOT WORKING')
+  const [Colorasthma, setColorAsthma] = useState('white')
   const [pollen, setPollen] = useState('NOT WORKING')
+  const [Colorpollen, setColorPollen] = useState('white')
   const [allergicRithics, setAllergicRithics] =useState('NOT WORKING')
+  const [ColorallergicRithics, setColorAllergicRithics] =useState('white')
   const [frostBite, setFrostBite] =useState('NOT WORKING')
-
+  const [ColorfrostBite, setColorFrostBite] =useState('white')
   
   useEffect(()=>{
 
@@ -59,17 +65,24 @@ useEffect(() => {
         const airQuality = ExtractCurrentAirQuality.so2
         if (airQuality< 3){
           setAsthma('Low')
+          setColorAsthma('green')
         }else if (airQuality>=3 & airQuality<7){
           setAsthma('Medium')
+          setColorAsthma('orange')
         } else if (airQuality>=7){
           setAsthma('High')
+          setColorAsthma('red')
+          
         }
         if (airQuality< 3){
           setAllergicRithics('Low')
+          setColorAllergicRithics('green')
         }else if (airQuality>=3 & airQuality<7){
           setAllergicRithics('Medium')
+          setColorAllergicRithics('orange')
         } else if (airQuality>=7){
           setAllergicRithics('High')
+          setColorAllergicRithics('red')
         }
 
         
@@ -109,43 +122,60 @@ useEffect(() => {
 
       if (temp> 15){
         setFluState('Low')
+        setColorFluState('green')
       }else if (temp>=0 && temp<=15){
 setFluState('Medium')
+setColorFluState('orange')
       } else if (temp< 0){
         setFluState('High')
+        setColorFluState('red')
       }
       if (temp> 10){
         setHyprothermia('Low')
+        setColorHyprothermia('green')
       }else if (temp< 10 && temp>0){
         setHyprothermia('Medium')
+        setColorHyprothermia('orange')
       } else if (temp < -10){
         setHyprothermia('High')
+        setColorHyprothermia('red')
       }
 
       if (temp< 30){
         setheatcramps('Low')
+        setColorheatcramps('green')
       }else if (temp> 30 && temp<45){
         setheatcramps('Medium')
+        setColorheatcramps('orange')
       } else if (temp> 45){
         setheatcramps('High')
+        setColorheatcramps('red')
+        
       }
       if (temp< 6){
         setPollen('Low')
+        setColorPollen('green')
       }else if (temp>= 6 && temp<=14){
         setPollen('Medium')
+        setColorPollen('orange')
       } else if (temp> 14){
         setPollen('High')
+        setColorPollen('red')
       }
 
       if (temp< -6){
         setFrostBite('Low')
+        setColorFrostBite('green')
       }else if (temp>= -6 && temp<=4){
         setFrostBite('Medium')
+        setColorFrostBite('orange')
       } else if (temp> 4){
         setFrostBite('High')
+        setColorFrostBite('red')
+        
       }
 
-      console.log(ColorColdurticariaState)
+      
 
       
 
@@ -156,12 +186,14 @@ setFluState('Medium')
     }
     handleHealthAdviceData()
   },[GlobalCityName,units])
-
+if (!isLoggedIn){
+ return  navigate('/Login')
+}
 
   return (
     <div className='HealthAdviceBakcground'>
       <div className='HealthAdvicecoverSection' style={{position:'absolute', top: '20%'}}>
-        <h1 className='allergytitle'>Allergies</h1>
+        <h1 className='allergytitle'>Allergies- Risk</h1>
       
         <div className='HealthAdviceSection'>
           
@@ -172,29 +204,29 @@ setFluState('Medium')
             <FaTree style={{width:'200px'}} />
 
             <h2>Pollen </h2>
-            <hr style={{width: '30%', height:'3%', backgroundColor:'white', marginLeft:'0px'}}/>
+            <hr style={{width: '30%', height:'5%', backgroundColor:Colorpollen, marginLeft:'0px',borderWidth:'0px'}}/>
             <h2>{pollen}</h2>
 
             </div>
             <div className='HealthAdviceBoxes'>
             <GiTumbleweed  />
             <h2>Cold urticaria </h2>
-            <hr style={{width: '30%', height:'3%', backgroundColor:ColorColdurticariaState, marginLeft:'0px'}}/>
+            <hr style={{width: '30%', height:'5%', backgroundColor:ColorColdurticariaState, marginLeft:'0px',borderWidth:'0px'}}/>
             <h2>{ColdurticariaState}</h2>
               
             </div>
             <div className='HealthAdviceBoxes'>
             <GiMoldova />
             <h2>Asthma </h2>
-            <hr style={{width: '30%', height:'3%', backgroundColor:'white', marginLeft:'0px'}}/>
+            <hr style={{width: '30%', height:'5%', backgroundColor:Colorasthma, marginLeft:'0px',borderWidth:'0px'}}/>
             <h2>{asthma}</h2>
               
             </div>
             
             <div className='HealthAdviceBoxes'>
             <GiGrass />
-            <h2>allergic rhinitis</h2>
-            <hr style={{width: '30%', height:'3%', backgroundColor:'white', marginLeft:'0px'}}/>
+            <h2>Allergic rhinitis</h2>
+            <hr style={{width: '30%', height:'5%', backgroundColor:ColorallergicRithics, marginLeft:'0px',borderWidth:'0px'}}/>
             <h2>{allergicRithics}</h2>
 
             </div>
@@ -205,7 +237,7 @@ setFluState('Medium')
 
       </div>
       <div className='HealthAdvicecoverSection' style={{position:'absolute', top: '60%'}}>
-        <h1 className='allergytitle'>Health</h1>
+        <h1 className='allergytitle'>Health- Risk</h1>
       
         <div className='HealthAdviceSection'>
           
@@ -216,29 +248,29 @@ setFluState('Medium')
             <FaTree style={{width:'200px'}} />
 
             <h2> Flu</h2>
-            <hr style={{width: '30%', height:'3%', backgroundColor:'white', marginLeft:'0px'}}/>
+            <hr style={{width: '30%', height:'5%', backgroundColor:ColorFluState, marginLeft:'0px',borderWidth:'0px'}}/>
             <h2>{FluState}</h2>
 
             </div>
             <div className='HealthAdviceBoxes'>
             <GiTumbleweed  />
-            <h2>hypothermia </h2>
-            <hr style={{width: '30%', height:'3%', backgroundColor:'white', marginLeft:'0px'}}/>
+            <h2>Hypothermia </h2>
+            <hr style={{width: '30%', height:'5%', backgroundColor:ColorHypothermiaState, marginLeft:'0px',borderWidth:'0px'}}/>
             <h2>{HypothermiaState}</h2>
               
             </div>
             <div className='HealthAdviceBoxes'>
             <GiMoldova />
-            <h2>frostbite </h2>
-            <hr style={{width: '30%', height:'3%', backgroundColor:'white', marginLeft:'0px'}}/>
+            <h2>Frostbite </h2>
+            <hr style={{width: '30%', height:'5%', backgroundColor:ColorfrostBite, marginLeft:'0px',borderWidth:'0px'}}/>
             <h2>{frostBite}</h2>
               
             </div>
             
             <div className='HealthAdviceBoxes'>
             <GiGrass />
-            <h2>heat cramps</h2>
-            <hr style={{width: '30%', height:'3%', backgroundColor:'white', marginLeft:'0px'}}/>
+            <h2>Heat cramps</h2>
+            <hr style={{width: '30%', height:'5%', backgroundColor:Colorheatcramps, marginLeft:'0px',borderWidth:'0px'}}/>
             <h2>{heatcramps}</h2>
 
             </div>
